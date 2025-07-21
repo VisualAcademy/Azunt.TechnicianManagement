e2b2b1d365bd7406030a60faa4b716badd63f89f
Park YongJune <redplus@redplus.net>
2025-07-21 12:18:34 +0900

    Add EF Core DbContext and factory

---
+++ src/Azunt.TechnicianManagement/Azunt.TechnicianManagement/03_Repositories/EfCore/TechnicianAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Azunt.TechnicianManagement
+{
+    public class TechnicianAppDbContext : DbContext
+    {
+        public TechnicianAppDbContext(DbContextOptions<TechnicianAppDbContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Technician>()
+                .Property(m => m.Created)
+                .HasDefaultValueSql("GetDate()");
+        }
+
+        public DbSet<Technician> Technicians { get; set; } = null!;
+    }
+}
---
+++ src/Azunt.TechnicianManagement/Azunt.TechnicianManagement/03_Repositories/EfCore/TechnicianAppDbContextFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Azunt.TechnicianManagement;
+
+public class TechnicianAppDbContextFactory
+{
+    private readonly IConfiguration? _configuration;
+
+    public TechnicianAppDbContextFactory() { }
+
+    public TechnicianAppDbContextFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public TechnicianAppDbContext CreateDbContext(string connectionString)
+    {
+        var options = new DbContextOptionsBuilder<TechnicianAppDbContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+        return new TechnicianAppDbContext(options);
+    }
+
+    public TechnicianAppDbContext CreateDbContext(DbContextOptions<TechnicianAppDbContext> options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+        return new TechnicianAppDbContext(options);
+    }
+
+    public TechnicianAppDbContext CreateDbContext()
+    {
+        if (_configuration == null)
+        {
+            throw new InvalidOperationException("Configuration is not provided.");
+        }
+
+        var defaultConnection = _configuration.GetConnectionString("DefaultConnection");
+
+        if (string.IsNullOrWhiteSpace(defaultConnection))
+        {
+            throw new InvalidOperationException("DefaultConnection is not configured properly.");
+        }
+
+        return CreateDbContext(defaultConnection);
+    }
+}
